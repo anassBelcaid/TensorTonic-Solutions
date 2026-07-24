b57c145265d6b74fbec29fa02217553e6f5298b0
anass <anass.belcaid@gmail.com>
2026-07-24 13:44:02 +0100

    Solve wasserstein-critic-loss

---
+++ wasserstein-critic-loss/wasserstein-critic-loss.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def wasserstein_critic_loss(real_scores, fake_scores):
+    """
+    Compute Wasserstein Critic Loss for WGAN.
+    """
+    # Write code here
+    real_scores = np.asarray(real_scores)
+    fake_scores = np.asarray(fake_scores)
+
+    return -np.mean(real_scores) + np.mean(fake_scores)
+
