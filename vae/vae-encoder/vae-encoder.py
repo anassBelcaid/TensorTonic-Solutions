5cf1b91b5fd138ad83a45551da2bdcae0048aa21
anass <anass.belcaid@gmail.com>
2026-09-21 20:36:01 +0100

    Solve vae-encoder

---
+++ vae/vae-encoder/vae-encoder.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def vae_encoder(
+    x: np.ndarray,
+    W_mu: np.ndarray,
+    b_mu: np.ndarray,
+    W_logvar: np.ndarray,
+    b_logvar: np.ndarray,
+) -> dict:
+    """
+    Returns mu and log_var as float64 arrays in a dictionary.
+    """
+    mu = x @ W_mu + b_mu
+    log_var = x @ W_logvar + b_logvar
+    return {"mu": mu, "log_var": log_var}
+
